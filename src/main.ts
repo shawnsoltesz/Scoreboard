eeeafc546620bd36943cdf334f62ab2935ef48be
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-09-01 22:49:36 -0400

    Team 1 + / - buttons displaying message when clicked

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import './style.css'
+
+let playerOneScore = 0
+let playerTwoScore = 0
+
+// // ScoreboardView
+
+// // const thingClickedOn = event.target
+
+// // constructor(playerOneName, playerTwoName, onScoreChange)
+
+function handleScoreClick() {
+  window.alert('You did it!')
+}
+
+// // inputElement.addEventListener('click', handleScoreClick)
+
+// // nameField.addEventListener('click', handleScoreClick)
+
+const addButton = document.querySelector('i.add')
+
+addButton.addEventListener('click', handleScoreClick)
+
+const deleteButton = document.querySelector('i.subtract')
+
+deleteButton.addEventListener('click', handleScoreClick)
